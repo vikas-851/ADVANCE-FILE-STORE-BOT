b32648692b83a3480c1df3f818b2f9803a80a4af
vikas-843 <119723483+vikas-843@users.noreply.github.com>
2024-01-03 05:58:02 +0530

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,42 @@
+#Coded by @KIRA_DEVELOPER
+
+from pyrogram import Client 
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "help":
+        await query.message.edit_text(
+            text = f"<b>Bot Cammands\n❏ Commands For BOT Admins\n├/start : start the bot or get posts\n├/batch : create link for more than one posts\n├/genlink : create link for one post\n├/users : view bot statistics\n├/broadcast : broadcast any messages to bot users\n└/stats : checking your bot uptime\n\n👨‍💻 Developed by <a href=https://t.me/Anime_Mayhem>Anime Mayhem</a></b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("💥​ᴄʟᴏꜱᴇ​💥", callback_data="close"),
+                        InlineKeyboardButton("⚡ᴀʙᴏᴜᴛ⚡", callback_data="about")
+                    ]
+                ]
+            )
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text = f"<b>Creator - @TANJIRO_X_CODER\nMy Channel - @TEAMS_VG\nDicsussion Group - @VG_SUPPORT_CHAT</a>\n\n👨‍💻 Developed by @KIRA_DEVELOPER</b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("💥​ᴄʟᴏꜱᴇ​💥", callback_data="close"),
+                        InlineKeyboardButton("🚀ʜᴇʟᴘ🚀", callback_data="help")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
